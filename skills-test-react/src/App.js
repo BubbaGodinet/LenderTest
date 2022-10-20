b7885a4bae6da063e63f8abb6178183bbc779eaa
Bubba Godinet <godinetvn@gmail.com>
2022-10-19 19:57:50 -0600

    lender test

--- skills-test-react/src/App.js
+++ skills-test-react/src/App.js
@@ -2,29 +2,43 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import AccountMenu from './AccountMenu';
+// import Link from '@mui/material/Link';
+import AccountMenu from './AccountMenu.js';
 import BasicForm from './BasicForm'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
 export default function App() {
+
+// 	require('react-dom');
+// window.React2 = require('react');
+// console.log(window.React1 === window.React2);
+
+
 	const [openForm, setOpenForm] = React.useState(false);
+	const navigate = useNavigate()
+
 	const handleOnClick = (page) => {
 		console.log('open');
 		if(!openForm) { 
 			setOpenForm(true)
+			navigate('/todo')
 		} else {
 			setOpenForm(false)
 		}
 	};
   return (
     <Container maxWidth="sm">
-	<AccountMenu onClick={handleOnClick} />
-	{openForm && <Basicorm />}
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          React Skills test
-        </Typography>
-      </Box>
+			<AccountMenu onClick={handleOnClick} />
+			<Routes>
+			{openForm &&  <Route path="/todo" element={<BasicForm />} />}
+      <Route path="/" element={
+				<Box sx={{ my: 4 }}>
+					<Typography variant="h4" component="h1" gutterBottom>
+						React Skills test
+					</Typography>
+				</Box>} 
+			/>
+			</Routes>
     </Container>
   );
 }
--- skills-test-react/src/BasicForm.js
+++ skills-test-react/src/BasicForm.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Formik, Field, Form } from 'formik';
-import { Button } from '@mui/material';
+// import ReactDOM from 'react-dom';
+import { Formik, Field, Form, FieldArray, ErrorMessage} from 'formik';
+import { Button, Divider, Container, TextField, ButtonGroup} from '@mui/material';
 
-const BasicForm = () => (
+const BasicForm = () => {
+
+const initialValues = {
+  todos: [
+    {
+      todo: '',
+    }
+  ]
+}
+
+  return (
   <div>
     <h1>TODO</h1>
     <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
+      enableReinitialize
+      initialValues={initialValues}
+      onSubmit={async (values) => {
+        await new Promise((resolve) => setTimeout(resolve, 400));
+        alert(JSON.stringify(values, null, 2));
       }}
-      onSubmit={(values) => {}}
     >
+      {({values}) => (
       <Form>
-        <label htmlFor="todo">Add ToDo </label>
-        <Field id="todo" name="Add ToDo" placeholder="TextHere" />
-        <button type="submit">Submit</button>
+              <FieldArray name="todos">
+            {({ insert, remove, push }) => (
+              <div>
+                {values.todos.length > 0 &&
+                  values.todos.map((todo, index) => (
+                    <>
+                    <Container className="row" key={index} sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                      <Container className="col" sx={{marginTop: '1.2vw'}}>
+                        <TextField
+                          sx={{width: '100%'}}
+                          name={`todos.${index}.todo`}
+                          placeholder="Add Todo"
+                          type="text"
+                        />
+                        <ErrorMessage
+                          name={`todos.${index}.todo`}
+                          component="div"
+                          className="field-error"
+                        />
+                      </Container>
+                      <div className="col">
+                      </div>
+                      <Container sx={{display: 'flex', gap: '3.472vw', justifyContent: 'center'}}>
+                        <Button
+                          sx={{marginBottom: '1.2vw', marginTop: '1.2vw', borderRadius: '3.472vw', backgroundColor: '#16BDF2', color: 'white', '&:hover': {color: 'white', backgroundColor: '#204EE6'}}}
+                          type="button"
+                          className="secondary"
+                          onClick={() => push({ todo: '' })}
+                        >
+                          Add Todo
+                        </Button>
+                        <Button
+                        sx={{marginBottom: '1.2vw', marginLeft: '0.694vw', marginTop: '1.2vw', borderRadius: '3.472vw', backgroundColor: '#FF3336', color: 'white', '&:hover': {color: 'white', backgroundColor: '#99080A'}}}
+                          type="button"
+                          className="secondary"
+                          onClick={() => remove(index)}
+                        >
+                          Delete
+                        </Button>
+                        </Container>
+                    </Container>
+                    <Divider/>
+                    </>
+                  ))}
+              </div>
+            )}
+          </FieldArray>
       </Form>
+      )}
     </Formik>
   </div>
-);
+  )
+}
 
 export default BasicForm
